ca5468d7fc026786287fb512a7a34524d342e64e
Matheus Stein Fontana <msfontana.dev@gmail.com>
2019-09-10 01:20:58 -0300

    Improvements

--- src/main/java/br/com/ibm/challenge/config/SwaggerConfig.java
+++ src/main/java/br/com/ibm/challenge/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package br.com.ibm.challenge;
+package br.com.ibm.challenge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
